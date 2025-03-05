86b62efefe41d3b2960d69ee0dfb5e18a516c4b9
Frank0125 <a01252974@tec.mx>
2025-03-05 00:34:55 -0700

    FIX: Changes in loop declaration, messages and logic

--- Practica2/main.cpp
+++ Practica2/main.cpp
@@ -17,21 +17,21 @@ int main() {
         if(cin.fail()) {
             throw invalid_argument("El tipo de valor introducido no es aceptado.");
         }
-        if(n >15 || n < 5) {
-            throw invalid_argument("El tamaño de lista de números a calificar debe no debe ser menor a " +
+        if(n > 15 || n < 5) {
+            throw invalid_argument("El tamaño de lista de números a calificar no debe ser menor a " +
             std::string("5 o mayor a 15."));
         }
 
         float *list = new float[n];
-        for (int i; i < n; i++){
-            cout << "Ingresa los números a calcular, uno por uno:";
+        for (int i = 0; i < n; i++) {
+            cout << "Ingresa un número para el calculo: " << endl;
             cin >> list[i];
             if (cin.fail()) {
                 throw invalid_argument("El tipo de valor introducido no es aceptado.");
             }
             if(list[i] > 100 || list[i] < 5) {
-                throw invalid_argument("El valor de número a calcular no debe ser menor a 5 o mayor a" +
-                std::string("100"));
+                throw invalid_argument("El valor del número a calcular no debe ser menor a 5 o mayor a" +
+                std::string(" 100"));
             }
         }
         cout<<"La desviación estándar de la lista dada es: " << c.standDevCalculator(list, n);
